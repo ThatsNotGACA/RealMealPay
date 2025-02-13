c4787867ac3e4b459fa9c982ef3a0c120cee7f46
Ethan Hoffman <thatsnotgaca@gmail.com>
2025-02-13 10:29:35 -0800

    Intial

--- Student.java
+++ Student.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class Student {
-    
+    private String name;
+    private int id;
+    private double balance;
+    private static int idGen = 1;
+    private ArrayList<Transaction> transactions;
+
+    public String toString(){
+        return "Name: " + name + " ID: " + id + " Balance: " + balance + " Transactions: " + transactions.toString();
+    }
+
+
 }
