9d265646275b1cbd31a85519855398b0bebfddfc
Ethan Hoffman <thatsnotgaca@gmail.com>
2025-02-23 10:23:42 -0800

    Student

--- Student.java
+++ Student.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Student {
     private String name;
@@ -7,9 +7,51 @@ public class Student {
     private static int idGen = 1;
     private ArrayList<Transaction> transactions;
 
-    public String toString(){
-        return "Name: " + name + " ID: " + id + " Balance: " + balance + " Transactions: " + transactions.toString();
+    public Student(String myName){
+        name = myName;
+        balance = 0;
+        id = idGen;
+        idGen++;
+        transactions = new ArrayList<Transaction>();
+    }
+
+    public void chargeLunch(int month, int day){
+        balance -= 7;
+        Transaction t = new Transaction(month, day, -7);
+        transactions.add(t);
+    }
+    public void addBalance(int month, int day, int amount){
+        balance+=amount;
+        Transaction t = new Transaction(month, day, amount);
+        transactions.add(t);
+    }
+
+    public ArrayList<Transaction> getTransactionsByDate(int month, int day){
+        ArrayList<Transaction> given = new ArrayList<Transaction>();
+
+        for(Transaction t : transactions){
+            if(month == t.getMonth() && day == t.getDay()){
+                given.add(t);
+            }
+        }
+        if(given.size() == 0){
+            return null;
+        } else {
+            return given;
+        }
     }
 
 
+    public String toString(){
+        return "Name: " + name + " ID: " + id + " Balance: " + balance + " Transactions: " + transactions.toString();
+    }
+    public String getName(){
+        return name;
+    }
+    public int getID(){
+        return id;
+    }
+    public double getBalance(){
+        return balance;
+    }
 }
